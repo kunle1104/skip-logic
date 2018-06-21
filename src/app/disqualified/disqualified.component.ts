f23ae260f7db9b4e3bf092ba58bd6e6b584b67f5
Adekunle Ishola <kunle1104@gmail.com>
2018-06-21 20:30:14 +1000

    Add files via upload

--- src/app/disqualified/disqualified.component.ts
+++ src/app/disqualified/disqualified.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-disqualified',
   templateUrl: './disqualified.component.html',
-  styleUrls: ['./disqualified.component.css']
+  //styleUrls: ['./disqualified.component.css']
 })
 export class DisqualifiedComponent implements OnInit {
 
